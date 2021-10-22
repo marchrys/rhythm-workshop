9d793e2a648d8a4b9dcbd4f265a514ac9aea8a5f
marchrys <mar.chrysikopoulos@gmail.com>
2021-10-22 21:46:06 +0900

    Ajout du select pour le choix de niveau.

--- www/js/settings.js
+++ www/js/settings.js
@@ -1,8 +1,20 @@
 const settings = new Vue({
   el: '#settings',
   data: {
+    lang: app.lang,
+    texts,
     patterns,
-    levels
+    levels,
+    selectedLevelId: 1
+  },
+  methods: {
+    initSelects: function(){
+      const elems = document.querySelectorAll('select');
+      const instances = M.FormSelect.init(elems, {});
+    }
+  },
+  mounted(){
+    this.initSelects();
   }
 });
 
--- www/js/texts.js
+++ www/js/texts.js
@@ -2,6 +2,10 @@ const texts = {
   appTitle: {
     en: 'Rhythm Workshop',
     fr: 'Atelier de rythme'
+  },
+  levelSelectLabel: {
+    en: 'Please select a level',
+    fr: 'Sélectionnez un niveau'
   }
 };
 
