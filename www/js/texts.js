8b464163ea20e3c20ebc54224375543279f92dbd
marchrys <mar.chrysikopoulos@gmail.com>
2021-10-24 15:44:59 +0900

    Ajout du preloader.

--- www/js/texts.js
+++ www/js/texts.js
@@ -6,6 +6,10 @@ const texts = {
   levelSelectLabel: {
     en: 'Please select a level',
     fr: 'Sélectionnez un niveau'
+  },
+  preloaderText: {
+    en: 'Please wait while loading sounds...',
+    fr: 'Veuillez patienter pendant le chargement des sons...'
   }
 };
 
